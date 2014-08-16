564d91f1d8f4022c94eac453417aedd73b47b237
Karan Goel <karan@goel.im>
2014-08-16 09:19:24 -0700

    more client code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,12 @@
+function showNext() {
+  console.log("clicked");
+  $.get('/give/-1', function(data) {
+    console.log(data);
+    $("#loader").attr('src', data['redirect_url'])
+  });
+};
+
 (function() {
 
   
-
 });
